d34fae63a646c6e3a3ff584d9a240a9dbc2642af
Ajyke <ajikeezichi@yahoo.com>
2023-02-28 19:52:38 +0100

    Election declaration V1

--- user.sh
+++ user.sh
@@ -1,1 +1,10 @@
-This one na trials oh
+#!/bin/bash
+#Announcemnet condition for election
+echo "candidate's result in percentage"
+read Abuja
+if [ $Abuja -lt 25 ];
+then 
+	echo "This candidate cannot be declared winner"
+else
+	echo "This candidate is eligible to be declared winner"
+fi
